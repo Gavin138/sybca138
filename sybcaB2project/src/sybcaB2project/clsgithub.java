b17875c34aaef2443cc7a27897d2cd4824a7500e
Gavin <rbca22138.rcca@gmail.com>
2024-01-15 10:51:11 +0530

    commit

--- sybcaB2project/src/sybcaB2project/clsgithub.java
+++ sybcaB2project/src/sybcaB2project/clsgithub.java
@@ -1,10 +1,22 @@
 package sybcaB2project;
-
+import java.util.Scanner;
 final class clsgithub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		int x, y,sum ;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("type a number:");
+		x = sc.nextInt();
+		
+		System.out.println("type another number:");
+		y = sc.nextInt();
+		
+		sum = x + y;
+		System.out.println("sum is: " +sum);
+		
 	}
 
 }
